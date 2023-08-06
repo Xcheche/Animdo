a93ed8dd451b805e596244a52bf6d7d68409845d
Xcheche <chekwubechukwu23@gmail.com>
2023-08-06 16:06:37 +0100

    animdo

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double _buttonRadius = 100;
+  Color _backgroundColor = Colors.blue;
+  final Tween<double> _backgroundScale = Tween<double>(begin: 0.0, end: 1.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            AnimatedContainer(
+              duration: const Duration(seconds: 2),
+              color: _backgroundColor,
+              curve: Curves.easeInOut,
+            ),
+            _circularAnimationButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _circularAnimationButton() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _buttonRadius += _buttonRadius == 200 ? -100 : 100;
+            _backgroundColor =
+                _backgroundColor == Colors.blue ? Colors.red : Colors.blue;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2),
+          curve: Curves.bounceInOut,
+          height: _buttonRadius,
+          width: _buttonRadius,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(_buttonRadius),
+          ),
+          child: const Center(
+            child: Text(
+              "Basic!",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
